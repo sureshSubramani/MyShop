d3c65da50d0bcd264ac99a4ab0380dd416b39f72
sureshSubramani <sureshsubramani1986@gmail.com>
2019-09-23 06:59:46 +0530

    2nd commit

--- restaurant.sql
+++ restaurant.sql
@@ -1,14 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Mar 01, 2018 at 03:05 PM
--- Server version: 5.6.35
--- PHP Version: 7.0.22
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2019 at 03:28 AM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.1.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `restaurant`
@@ -25,6 +33,15 @@ CREATE TABLE `category` (
   `name` varchar(255) NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `active`) VALUES
+(1, 'Men', 1),
+(2, 'Women', 1),
+(3, 'Shirts', 1);
 
 -- --------------------------------------------------------
 
@@ -49,7 +66,7 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`id`, `company_name`, `service_charge_value`, `vat_charge_value`, `address`, `phone`, `country`, `message`, `currency`) VALUES
-(1, 'Lulu', '', '13', 'READING', '234234235', 'Spai', 'this is just an testing', 'USD');
+(1, 'CMS', '', '0', 'READING', '234234235', 'Spai', 'this is just an testing', 'INR');
 
 -- --------------------------------------------------------
 
@@ -69,8 +86,8 @@ CREATE TABLE `groups` (
 
 INSERT INTO `groups` (`id`, `group_name`, `permission`) VALUES
 (1, 'Super Administrator', 'a:32:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createStore\";i:9;s:11:\"updateStore\";i:10;s:9:\"viewStore\";i:11;s:11:\"deleteStore\";i:12;s:11:\"createTable\";i:13;s:11:\"updateTable\";i:14;s:9:\"viewTable\";i:15;s:11:\"deleteTable\";i:16;s:14:\"createCategory\";i:17;s:14:\"updateCategory\";i:18;s:12:\"viewCategory\";i:19;s:14:\"deleteCategory\";i:20;s:13:\"createProduct\";i:21;s:13:\"updateProduct\";i:22;s:11:\"viewProduct\";i:23;s:13:\"deleteProduct\";i:24;s:11:\"createOrder\";i:25;s:11:\"updateOrder\";i:26;s:9:\"viewOrder\";i:27;s:11:\"deleteOrder\";i:28;s:10:\"viewReport\";i:29;s:13:\"updateCompany\";i:30;s:11:\"viewProfile\";i:31;s:13:\"updateSetting\";}'),
-(4, 'Members', 'a:9:{i:0;s:9:\"viewStore\";i:1;s:11:\"deleteStore\";i:2;s:9:\"viewTable\";i:3;s:11:\"deleteTable\";i:4;s:12:\"viewCategory\";i:5;s:11:\"viewProduct\";i:6;s:11:\"createOrder\";i:7;s:11:\"updateOrder\";i:8;s:9:\"viewOrder\";}'),
-(5, 'Staff', 'a:6:{i:0;s:9:\"viewTable\";i:1;s:11:\"viewProduct\";i:2;s:11:\"createOrder\";i:3;s:11:\"updateOrder\";i:4;s:9:\"viewOrder\";i:5;s:11:\"viewProfile\";}');
+(4, 'Members', 'a:9:{i:0;s:9:\"viewStore\";i:1;s:9:\"viewTable\";i:2;s:12:\"viewCategory\";i:3;s:11:\"viewProduct\";i:4;s:11:\"createOrder\";i:5;s:11:\"updateOrder\";i:6;s:9:\"viewOrder\";i:7;s:11:\"viewProfile\";i:8;s:13:\"updateSetting\";}'),
+(5, 'Staff', 'a:12:{i:0;s:8:\"viewUser\";i:1;s:9:\"viewStore\";i:2;s:9:\"viewTable\";i:3;s:12:\"viewCategory\";i:4;s:13:\"createProduct\";i:5;s:13:\"updateProduct\";i:6;s:11:\"viewProduct\";i:7;s:11:\"createOrder\";i:8;s:11:\"updateOrder\";i:9;s:9:\"viewOrder\";i:10;s:11:\"viewProfile\";i:11;s:13:\"updateSetting\";}');
 
 -- --------------------------------------------------------
 
@@ -95,6 +112,14 @@ CREATE TABLE `orders` (
   `store_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `bill_no`, `date_time`, `gross_amount`, `service_charge_rate`, `service_charge_amount`, `vat_charge_rate`, `vat_charge_amount`, `discount`, `net_amount`, `user_id`, `table_id`, `paid_status`, `store_id`) VALUES
+(1, 'BILPR-E316', '1568924767', '1800.00', '', '0', '13', '234.00', '', '2034.00', 1, 2, 1, 1),
+(2, 'BILPR-859B', '1569048835', '1760.00', '', '0', '0', '0', '228.80', '1760.00', 1, 3, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -109,6 +134,15 @@ CREATE TABLE `order_items` (
   `rate` varchar(255) NOT NULL,
   `amount` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `qty`, `rate`, `amount`) VALUES
+(1, 1, 3, '2', '550', '1100.00'),
+(2, 1, 2, '2', '350', '700.00'),
+(6, 2, 4, '1', '1760', '1760.00');
 
 -- --------------------------------------------------------
 
@@ -127,6 +161,15 @@ CREATE TABLE `products` (
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `store_id`, `name`, `price`, `description`, `image`, `active`) VALUES
+(1, '[\"1\"]', '[\"1\"]', 'Sandal', '350', '<p>smooth walking</p>', 'assets/images/product_image/5d83e2179262a.jpg', 1),
+(2, '[\"1\"]', '[\"1\"]', 'Sandal', '350', '<p>Smooth walking...</p>', 'assets/images/product_image/5d83e2d94e912.jpg', 1),
+(4, '[\"1\"]', '[\"1\"]', 'Casual Shoe', '1760', '<p>Smooth and with 1 Year Warranty</p>', 'assets/images/product_image/5d85c84158b22.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -138,6 +181,14 @@ CREATE TABLE `stores` (
   `name` varchar(255) NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stores`
+--
+
+INSERT INTO `stores` (`id`, `name`, `active`) VALUES
+(1, 'Store1', 1),
+(2, 'Store2', 1);
 
 -- --------------------------------------------------------
 
@@ -153,6 +204,14 @@ CREATE TABLE `tables` (
   `active` int(11) NOT NULL,
   `store_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`id`, `table_name`, `capacity`, `available`, `active`, `store_id`) VALUES
+(2, 'tbl1', '400', 2, 1, 1),
+(3, 'tbl2', '200', 2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -177,7 +236,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `firstname`, `lastname`, `phone`, `gender`, `store_id`) VALUES
-(1, 'admin', '$2y$10$yfi5nUQGXUZtMdl27dWAyOd/jMOmATBpiUvJDmUu9hJ5Ro6BE5wsK', 'admin@admin.com', 'john', 'doe', '80789998', 1, 0);
+(1, 'admin', '$2y$10$ldv.TieUsuyjthZ6edb34ulpmZeNycKM.YmqAmhLfdFUAv27mH4oy', 'admin@admin.com', 'SURESH', 'SUBRAMANI', '088840 74278', 1, 1),
+(3, 'suresh', '$2y$10$zd4LqzxvTEM6Z8McY9awletyHIhP4tj7whLEx3M1ADSSEPV8a1762', 'sureshrameshmca@gmail.com', 'suresh', 'subramani', '088840 74278', 1, 1),
+(4, 'karpagam', '$2y$10$C5UfR0zx1WtlWQ9AgDduMuDdYjnROGaA61LHPLdgN.5Vt73rUFmpy', 'sureshkarpagam1986@gmail.com', 'suresh', 'subramani', '088840 74278', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -196,7 +257,10 @@ CREATE TABLE `user_group` (
 --
 
 INSERT INTO `user_group` (`id`, `user_id`, `group_id`) VALUES
-(1, 1, 1);
+(1, 1, 1),
+(2, 2, 5),
+(3, 3, 5),
+(4, 4, 4);
 
 --
 -- Indexes for dumped tables
@@ -270,49 +334,63 @@ ALTER TABLE `user_group`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `tables`
 --
 ALTER TABLE `tables`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `user_group`
 --
 ALTER TABLE `user_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
